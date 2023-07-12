71133b9f166cfbc2336b38c620f195a5b50cec25
AkankshaAnil <akankshakodgirwar07@gmail.com>
2023-07-12 15:06:57 +0530

    [AkankshaAnil] adding functionality for single player at start position 0

---
+++ SnakeAndLadder/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace singleAndLadder;
+using SnakeAndLadder;
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to Snake and Ladder Problem.");
+        singlePlayer snakeAndLadder = new singlePlayer();
+    }
+}
---
+++ SnakeAndLadder/singlePlayer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    public class singlePlayer
+    {
+            public singlePlayer()
+            {
+                Console.WriteLine("For Single Player");
+                int position = 0;
+                Console.WriteLine("Current position: " + position);
+            }
+        }
+    }
+
